69271cf812b10d0a183d8d007c388095fcee29d4
Dmitry177777 <k1791@mail.ru>
2024-01-08 16:29:35 +0500

    flake8 main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 
-from main import views
 from main.apps import MainConfig
 from rest_framework.routers import DefaultRouter
 
-from main.views import (HabitGuideViewSet, HabitUserCreateAPIView, HabitUserListAPIView, HabitUserRetrieveAPIView,
-                        HabitUserUpdateAPIView, HabitUserDestroyAPIView)
+from main.views import (
+    HabitGuideViewSet,
+    HabitUserCreateAPIView,
+    HabitUserListAPIView,
+    HabitUserRetrieveAPIView,
+    HabitUserUpdateAPIView,
+    HabitUserDestroyAPIView
+)
 
 app_name = MainConfig.name
 
@@ -13,10 +18,21 @@ router = DefaultRouter()
 router.register(r'Habit', HabitGuideViewSet, basename='habit')
 
 
-urlpatterns = [path('habit_user/create/', HabitUserCreateAPIView.as_view(), name='habit_user-create'),
-               path('habit_user/', HabitUserListAPIView.as_view(), name='habit_user-list'),
-               path('habit_user/<int:pk>/', HabitUserRetrieveAPIView.as_view(), name='habit_user-get'),
-               path('habit_user/update/<int:pk>/', HabitUserUpdateAPIView.as_view(), name='habit_user-update'),
-               path('habit_user/delete/<int:pk>/', HabitUserDestroyAPIView.as_view(), name='habit_user-delete'),
+urlpatterns = [
+    path('habit_user/create/',
+         HabitUserCreateAPIView.as_view(),
+         name='habit_user-create'),
+    path('habit_user/',
+         HabitUserListAPIView.as_view(),
+         name='habit_user-list'),
+    path('habit_user/<int:pk>/',
+         HabitUserRetrieveAPIView.as_view(),
+         name='habit_user-get'),
+    path('habit_user/update/<int:pk>/',
+         HabitUserUpdateAPIView.as_view(),
+         name='habit_user-update'),
+    path('habit_user/delete/<int:pk>/',
+         HabitUserDestroyAPIView.as_view(),
+         name='habit_user-delete'),
 
-               ]+router.urls
+   ]+router.urls
