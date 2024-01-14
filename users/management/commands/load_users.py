c8a9fe23863eb5615e894b91c304a8066b18368b
Dmitry177777 <k1791@mail.ru>
2024-01-14 21:09:01 +0500

    flake8 load_users.py

--- users/management/commands/load_users.py
+++ users/management/commands/load_users.py
@@ -3,43 +3,35 @@ from users.models import User
 
 
 class Command(BaseCommand):
-	def handle (self, *args, **options):
-
-
+	def handle(self, *args, **options):
 		# очистка модели User
 		records = User.objects.all()
 		records.delete()
 
-
 		# создание суперюзера
 		user = User.objects.create(
 			email='admin@sky.pro',
-			first_name = 'admin',
-			last_name = 'SkyPro',
-			is_staff = True,
-			is_superuser = True,
-			is_active = True
+			first_name='admin',
+			last_name='SkyPro',
+			is_staff=True,
+			is_superuser=True,
+			is_active=True
 		)
 
 		user.set_password('admin171717')
 		user.save()
 
-
-
-
 		user = User.objects.create(
 				email='k1779@mail.ru',
-				first_name = 'userMail',
-				last_name = 'mailRu',
-				is_staff = True,
-				is_superuser = False,
-				is_active = True
+				first_name='userMail',
+				last_name='mailRu',
+				is_staff=True,
+				is_superuser=False,
+				is_active=True
 			)
 
 		user.set_password('userMail')
 		user.save()
-
-
 
 		user = User.objects.create(
 			email='k17911971@yandex.ru',
