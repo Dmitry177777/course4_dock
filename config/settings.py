83ff3dd1a39a518126ad2c9def6dff81934b83fb
Dmitry177777 <k1791@mail.ru>
2024-01-14 20:29:27 +0500

    flake8 config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -13,22 +13,16 @@ import os
 from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
-from django.conf.global_settings import LOGIN_URL
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-ix_ge=(r_nl8*s5*2w5f6jkn%t*^q(o=z8etg=9fhj)!2w-z*h'
-
 load_dotenv()
 
-SECRET_KEY = os.environ.get('SECRET_KEY')  # take environment variables from .env.
+SECRET_KEY = os.environ.get('SECRET_KEY')
+# take environment variables from .env.
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -49,20 +43,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #install
+    # install
     'rest_framework',
     'rest_framework_simplejwt',
 
-    #документация
+    # документация
     'drf_yasg',
 
-    #celery
+    # celery
     'django_celery_beat',
 
-    #cors
+    # cors
     'corsheaders',
 
-    #myapp
+    # myapp
     'users',
     'main',
 ]
@@ -103,7 +97,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 # take environment variables from .env.
 BD_ENGINE = os.environ.get('BD_ENGINE')
 BD_NAME = os.environ.get('BD_NAME')
@@ -112,10 +105,6 @@ BD_PASSWORD = os.environ.get('BD_PASSWORD')
 BD_HOST = os.environ.get('BD_HOST')
 BD_PORT = os.environ.get('BD_PORT')
 
-
-
-# PostgreSQL_Key = os.getenv('PostgreSQL_Key')
-# TELEGRAM_BOT_API_KEY=os.getenv('TELEGRAM_BOT_API_KEY')
 
 DATABASES = {
     'default': {
@@ -129,24 +118,15 @@ DATABASES = {
 }
 
 
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.'
+             'password_validation.UserAttributeSimilarityValidator', },
+    {'NAME': 'django.contrib.auth.'
+             'password_validation.MinimumLengthValidator', },
+    {'NAME': 'django.contrib.auth.'
+             'password_validation.CommonPasswordValidator', },
+    {'NAME': 'django.contrib.auth.'
+             'password_validation.NumericPasswordValidator', },
 ]
 
 # Настройки JWT-токенов
@@ -166,13 +146,13 @@ SIMPLE_JWT = {
 }
 
 STRIPE_CREATE_URL = 'https://api.stripe.com/v1/payment_intents'
-STRIPE_RETRIEVE_URL = 'https://api.stripe.com/v1/payment_intents/pi_1Gt09Z2eZvKYlo2C8ZiS4b2r'
-STRIPE_AUTH ='pk_test_51Nnm8HFlTYpLw2PHisnKZg3IOPQIqzPH8YJeZ6LGBtrBs6IcYJFNukq638cCIg9f6qhnoEVpJwwGBvJCIIo5duPT005Jyp2pZy'
+STRIPE_RETRIEVE_URL = ('https://api.stripe.com/v1/'
+                       'payment_intents/pi_1Gt09Z2eZvKYlo2C8ZiS4b2r')
+STRIPE_AUTH = ('pk_test_51Nnm8HFlTYpLw2PHisnKZg3IOPQIqzPH8YJeZ6L'
+               'GBtrBs6IcYJFNukq638cCIg9f6qhnoEVpJwwGBvJCIIo5duPT005Jyp2pZy')
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -183,16 +163,14 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-############ redis ##############
+# redis
 
 CACHES = {
     "default": {
@@ -212,7 +190,8 @@ REDIS_PORT = load_dotenv('REDIS_PORT')
 CELERY_BEAT_SCHEDULE = {
     'check_last_login': {
         'task': 'main.tasks_celery.check_periodicity',  # Путь к задаче
-        'schedule': timedelta(minutes=1),  # Расписание выполнения задачи (например, каждые сутки)
+        'schedule': timedelta(minutes=1),
+        # Расписание выполнения задачи (например, каждые сутки)
     },
 }
 
@@ -220,7 +199,7 @@ CELERY_BEAT_SCHEDULE = {
 CELERY_BROKER_URL = load_dotenv('CELERY_BROKER_URL')
 CELERY_RESULT_BACKEND = load_dotenv('CELERY_RESULT_BACKEND')
 
-#Часовой пояс для работы Celery
+# Часовой пояс для работы Celery
 CELERY_TIMEZONE = "Australia/Tasmania"
 
 # Флаг отслеживания выполнения задач
@@ -229,11 +208,16 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30*60
 
-#CORS
-CORS_ALLOWED_ORIGINS = [  'https://localhost:8000',  # Замените на адрес вашего фронтенд-сервера  #'http://localhost:8000'  # это адрес вашего фронтенд-сервера. Замените его на адрес своего фронтенд-сервера или '*' , если вы хотите разрешить запросы от любого домена.
-]
+# CORS
+CORS_ALLOWED_ORIGINS = ['https://localhost:8000', ]
+# Замените на адрес вашего фронтенд-сервера
+# #'http://localhost:8000'
+# это адрес вашего фронтенд-сервера.
+# Замените его на адрес своего фронтенд-сервера или
+# '*' , если вы хотите разрешить запросы от любого домена.
 
-CSRF_TRUSTED_ORIGINS = [ "https://read-and-write.example.com", #  Замените на адрес вашего фронтенд-сервера   # и добавьте адрес бэкенд-сервера
-]
+CSRF_TRUSTED_ORIGINS = ["https://read-and-write.example.com", ]
+#  Замените на адрес вашего фронтенд-сервера
+# и добавьте адрес бэкенд-сервера]
 
 CORS_ALLOW_ALL_ORIGINS = True
