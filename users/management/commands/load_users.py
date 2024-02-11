e8e6bcfdcf0a69c460e983993d391b234504f520
Dmitry177777 <k1791@mail.ru>
2024-02-11 11:43:52 +0500

    flake8 load_users.py+

--- users/management/commands/load_users.py
+++ users/management/commands/load_users.py
@@ -1,46 +1,47 @@
 from django.core.management import BaseCommand
+
 from users.models import User
 
 
 class Command(BaseCommand):
-	def handle(self, *args, **options):
-		# очистка модели User
-		records = User.objects.all()
-		records.delete()
+    def handle(self, *args, **options):
+        # очистка модели User
+        records = User.objects.all()
+        records.delete()
 
-		# создание суперюзера
-		user = User.objects.create(
-			email='admin@sky.pro',
-			first_name='admin',
-			last_name='SkyPro',
-			is_staff=True,
-			is_superuser=True,
-			is_active=True
-		)
+        # создание суперюзера
+        user = User.objects.create(
+          email='admin@sky.pro',
+          first_name='admin',
+          last_name='SkyPro',
+          is_staff=True,
+          is_superuser=True,
+          is_active=True
+        )
 
-		user.set_password('admin171717')
-		user.save()
+        user.set_password('admin171717')
+        user.save()
 
-		user = User.objects.create(
-				email='k1779@mail.ru',
-				first_name='userMail',
-				last_name='mailRu',
-				is_staff=True,
-				is_superuser=False,
-				is_active=True
-			)
+        user = User.objects.create(
+          email='k1779@mail.ru',
+          first_name='userMail',
+          last_name='mailRu',
+          is_staff=True,
+          is_superuser=False,
+          is_active=True
+        )
 
-		user.set_password('userMail')
-		user.save()
+        user.set_password('userMail')
+        user.save()
 
-		user = User.objects.create(
-			email='k17911971@yandex.ru',
-			first_name='userYandex',
-			last_name='YandexRu',
-			is_staff=True,
-			is_superuser=False,
-			is_active=True
-		)
+        user = User.objects.create(
+          email='k17911971@yandex.ru',
+          first_name='userYandex',
+          last_name='YandexRu',
+          is_staff=True,
+          is_superuser=False,
+          is_active=True
+        )
 
-		user.set_password('userYandex')
-		user.save()
+        user.set_password('userYandex')
+        user.save()
